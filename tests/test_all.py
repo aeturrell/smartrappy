bf1b36a1785c83ba05310582e8873e7dc24ba5af
aeturrell <a.e.turrell@gmail.com>
2025-02-19 23:55:50 +0000

    outputs from tests in sensible place

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,6 +1,6 @@
 from smartrappy import analyze_and_visualize
 
 # Analyze the project directory
-analyze_and_visualize("tests/test_set_two", "tests/test_output/dag")
+analyze_and_visualize("tests/test_set_two", "tests/test_output/dagone")
 
-analyze_and_visualize("tests/test_set_one", "tests/test_output/dag")
+analyze_and_visualize("tests/test_set_one", "tests/test_output/dagtwo")
